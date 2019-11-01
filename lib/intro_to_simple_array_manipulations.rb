4ef34525103cbc1d5f4633e83e6fce1ffbfdb03d
dwane richards <dwanerich@gmail.com>
2019-11-01 22:59:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,50 +1,102 @@
+
+
 def using_push(array, string)
-  new_ray = string
-  array.push(new_ray)
+  new_ele = string
+  array.push(new_ele)
 end
+
+
+
+
+
 
 def using_unshift(array, string)
   new_ray = string
   array.unshift(new_ray)
 end
 
+
+
+
+
+
+
 def using_pop(array)
   p array.pop
 end
+
+
+
+
 
 def pop_with_args(array, 2)
   p array.pop[2]
 end
 
+
+
+
 def using_shift(array)
   p array.shift
 end
 
+
+
+
+
 def shift_with_args(array, 2)
   p array.shift[2]
 end
+
+
+
+
+
 
 def using_concat(array1, array2)
   array2.concat = (array1 + array2)
   p array2
 end
 
+
+
+
+
 def using_insert(array, element)
   p array.insert[4] = element
 end
+
+
+
+
 
 def using_uniq(array)
   p array.uniq
 end
 
+
+
+
+
+
 def using_flatten(array)
     p array.flatten
   end
+
+
+
+
+
 
 def using_delete(array, string)
     array.delete(string)
     
 end
+
+
+
+
+
 
 def using_delete_at(array, 2)
     array.delete[2]
